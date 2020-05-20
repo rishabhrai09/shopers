9284fb8c7c7faf94afd317ab9953538edef5bd8b
rishabhrai09 <rishabh8526@gmail.com>
2020-05-20 15:14:04 +0530

    Updates

---
+++ precache-manifest.111d6ca337b0313a60d7e00bdca4de25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0b42f164c68c40f07b3d75863cecdd",
+    "url": "/shopperStop/index.html"
+  },
+  {
+    "revision": "a995d03a681fae0f8590",
+    "url": "/shopperStop/static/css/main.35b8339d.chunk.css"
+  },
+  {
+    "revision": "e96e9d7ebcf5518a53b8",
+    "url": "/shopperStop/static/js/2.2f526b56.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopperStop/static/js/2.2f526b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a995d03a681fae0f8590",
+    "url": "/shopperStop/static/js/main.7a5aa0de.chunk.js"
+  },
+  {
+    "revision": "a23a504ede07e542a511",
+    "url": "/shopperStop/static/js/runtime-main.f951bfb8.js"
+  }
+]);
